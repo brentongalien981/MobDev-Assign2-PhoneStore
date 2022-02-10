f1962e8d3060881620945a820529ae053523a63f
odox700 <brentongalien981@gmail.com>
2022-02-10 02:49:28 -0500

    Did BrandActivity

---
+++ app/src/main/java/com/example/brenbaga_comp304_001_assign2/BrandActivity.java
@@ -0,0 +1,54 @@
+package com.example.brenbaga_comp304_001_assign2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BrandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brand);
+    }
+
+
+    // Set the options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_brands, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int brandImageResourceId = R.drawable.brand_iphone;
+
+        switch (item.getItemId()) {
+            case R.id.Samsung:
+                brandImageResourceId = R.drawable.brand_samsung;
+                break;
+            case R.id.GooglePixel:
+                brandImageResourceId = R.drawable.brand_google;
+                break;
+            case R.id.Huawei:
+                brandImageResourceId = R.drawable.brand_huawei;
+                break;
+        }
+
+        // Set BrandTextView.
+        TextView brandNameTextView = (TextView) findViewById(R.id.brandNameTextView);
+        brandNameTextView.setText(item.getTitle());
+
+        // Set BrandImageView.
+        ImageView iv = (ImageView) findViewById(R.id.phoneBrandImageView);
+        iv.setImageResource(brandImageResourceId);
+
+        return true;
+    }
+}
--- app/src/main/java/com/example/brenbaga_comp304_001_assign2/MainActivity.java
+++ app/src/main/java/com/example/brenbaga_comp304_001_assign2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.brenbaga_comp304_001_assign2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void handleBuyNow(View view) {
+
+        // Init intent.
+        Intent myIntent = new Intent(this, BrandActivity.class);
+
+
+        // Go to intent.
+        startActivity(myIntent);
     }
 }
